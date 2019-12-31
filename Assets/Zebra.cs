8b9b7747d47a8f01931871013b82fc91c9d81b7f
MAURICE\User <brendan.j.brown@hotmail.com>
2019-12-31 14:16:57 +1100

    made zebra scene, set up walk and rotate function an d made grass for the zebra to interact with. camera also tracks the zebra

---
+++ Assets/Zebra.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zebra : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 100f;
+    [SerializeField] float walkSpeed = 100f;
+
+    Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Turn();
+        WalkDirection();
+    }
+
+    private void Turn()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = turnSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.down * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;
+    }
+
+    private void WalkDirection()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            float thrustThisFrame = walkSpeed * Time.deltaTime;
+
+            transform.position -= transform.forward * thrustThisFrame;
+            
+        }
+        else if(Input.GetKey(KeyCode.S))
+        { 
+            float thrustThisFrame = walkSpeed * Time.deltaTime;
+
+            transform.position += transform.forward * thrustThisFrame;
+        }
+        
+    }
+}
