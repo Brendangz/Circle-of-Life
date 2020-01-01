e02caa519f3b92506c190263968bef000acdb139
MAURICE\User <brendan.j.brown@hotmail.com>
2020-01-01 13:44:13 +1100

    set up main menu with buttons and a randomise script that does not work yet

---
+++ Assets/RandomiseScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomiseScene : MonoBehaviour
+{
+    
+    int levelRange = Random.Range(1, 2);
+
+
+    private void Start()
+    {
+        OnMouseClick();
+    }
+
+    public void OnMouseClick()
+    {
+        SceneManager.LoadScene(levelRange);
+    }
+}
+
+
